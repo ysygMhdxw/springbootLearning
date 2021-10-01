dd8f5b8c94c605b4b5b531135cf87b05df421a8a
ysygMhdxw <75829561+ysygMhdxw@users.noreply.github.com>
2021-10-02 02:09:20 +0800

    Initial commit

---
+++ src/main/java/com/example/learn_pro1/LearnPro1Application.java
@@ -0,0 +1,13 @@
+package com.example.learn_pro1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnPro1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnPro1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learn_pro1/LearnPro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_pro1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnPro1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
